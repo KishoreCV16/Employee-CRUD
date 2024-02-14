c89469a990e2e903bc72d5ac6813095293bf94a9
Kishore Reddy <kishorecrgp@gmail.com>
2024-02-15 01:54:40 +0530

    Added Intermediate level features

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,17 +1,20 @@
 package com.example.demo.service;
 
-import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
 
 import com.example.demo.model.Employee;
 
 public interface EmployeeService {
     
-    List<Employee> getEmployees();
+	Page<Employee> getAllEmployees(int pageNumber, int pageSize, String sortBy);
     
     UUID saveEmployee(Employee employee);
     
     void deleteEmployee(UUID id);
     
     Employee updateEmployee(Employee employee);
+    
+    Employee getNthLevelManager(UUID employeeId, int level);
 }
